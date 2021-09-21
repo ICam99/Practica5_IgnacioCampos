31c784d2929defde926c7f6e1d76ad9ba4d02ee2
Nacho <ignacio.campos@potros.itson.edu.mx>
2021-09-21 16:47:29 -0700

    Initial commit

---
+++ app/src/main/java/campos/ignacio/thecheezery/RegisterActivity.kt
@@ -0,0 +1,21 @@
+package campos.ignacio.thecheezery
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+
+class RegisterActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_register)
+
+        val button : Button = findViewById(R.id.btn_get_started)
+
+        button.setOnClickListener {
+            var intent: Intent = Intent(this,MenuActivity::class.java)
+            startActivity(intent)
+        }
+
+    }
+}
